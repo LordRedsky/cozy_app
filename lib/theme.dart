567f19cd211e8076001e12e05266dc08c7e79da6
lordRedsky <aminmuhammadrizki@gmail.com>
2023-02-19 09:01:17 +0800

    setup text style

---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color purpleColor = Color(0xff5843BE);
+Color orangeColor = Color(0xffFF9376);
+Color blackColor = Color(0xff000000);
+Color whiteColor = Color(0xffFFFFFF);
+Color greyColor = Color(0xff82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
