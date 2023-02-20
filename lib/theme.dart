1df164a4fb9b78924d14464a51842fe10623d47c
lordRedsky <aminmuhammadrizki@gmail.com>
2023-02-20 15:05:39 +0800

    create splash page

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+//!COLOR
 Color purpleColor = Color(0xff5843BE);
 Color orangeColor = Color(0xffFF9376);
 Color blackColor = Color(0xff000000);
 Color whiteColor = Color(0xffFFFFFF);
 Color greyColor = Color(0xff82868E);
 
+
+//!TEXT STYLE
 TextStyle blackTextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: blackColor,
