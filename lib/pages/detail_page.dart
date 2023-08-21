26db634159304f35fb617f8c14bd0e1db581f208
lordRedsky <aminmuhammadrizki@gmail.com>
2023-08-21 16:12:24 +0800

    progress in facilities item

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,3 +1,5 @@
+import 'package:cozy_app/theme.dart';
+import 'package:cozy_app/widgets/facility_item.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
@@ -8,7 +10,174 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         bottom: false,
-        child: Text('Detail page'),
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/thumbnail.png',
+              width: MediaQuery.of(context).size.width,
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: edge,
+                vertical: 30,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'assets/btn_back.png',
+                      width: 40,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/btn_wishlist.png',
+                    width: 40,
+                  ),
+                ],
+              ),
+            ),
+            ListView(
+              children: [
+                const SizedBox(
+                  height: 328,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  // height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      //! NOTE: TITTLE
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: edge),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Kuretakeso Hott',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$52',
+                                    style: purpleTextStyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: ' / month',
+                                        style: greyTextStyle.copyWith(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/star_icon.png',
+                                  width: 20,
+                                ),
+                                const SizedBox(width: 2),
+                                Image.asset(
+                                  'assets/star_icon.png',
+                                  width: 20,
+                                ),
+                                const SizedBox(width: 2),
+                                Image.asset(
+                                  'assets/star_icon.png',
+                                  width: 20,
+                                ),
+                                const SizedBox(width: 2),
+                                Image.asset(
+                                  'assets/star_icon.png',
+                                  width: 20,
+                                ),
+                                const SizedBox(width: 2),
+                                Image.asset(
+                                  'assets/star_icon.png',
+                                  width: 20,
+                                  color: Color(0xff989BA1),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      //! NOTE: MAIN FACILITIES
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Main Facilities',
+                          style: regularTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                              name: 'kitchen',
+                              imageUrl: 'assets/icon_kitchen.png',
+                              total: 2,
+                            ),
+                            FacilityItem(
+                              name: 'bedroom',
+                              imageUrl: 'assets/icon_bed.png',
+                              total: 3,
+                            ),
+                            FacilityItem(
+                              name: 'big lemari',
+                              imageUrl: 'assets/icon_cupboard.png',
+                              total: 3,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
